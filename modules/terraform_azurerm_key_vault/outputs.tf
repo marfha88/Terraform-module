4031e44ab37970e40c4eb58a6334d37c39c00f61
Martin <martin.fahlbeck@softwareone.com>
2026-08-11 11:33:36 +0200

    fix output

--- modules/terraform_azurerm_key_vault/outputs.tf
+++ modules/terraform_azurerm_key_vault/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "The ID of the Key Vault."
-  value       = azurerm_key_vault.this.id
+  value       = azurerm_key_vault.kv.id
 }
 
 output "name" {
   description = "The name of the Key Vault."
-  value       = azurerm_key_vault.this.name
+  value       = azurerm_key_vault.kv.name
 }
 
 output "vault_uri" {
   description = "The URI of the Key Vault."
-  value       = azurerm_key_vault.this.vault_uri
+  value       = azurerm_key_vault.kv.vault_uri
 }
--- modules/terraform_azurerm_storage_account/outputs.tf
+++ modules/terraform_azurerm_storage_account/outputs.tf
@@ -1,26 +1,26 @@
 output "id" {
   description = "The ID of the storage account."
-  value       = azurerm_storage_account.this.id
+  value       = azurerm_storage_account.sa.id
 }
 
 output "name" {
   description = "The name of the storage account."
-  value       = azurerm_storage_account.this.name
+  value       = azurerm_storage_account.sa.name
 }
 
 output "primary_blob_endpoint" {
   description = "The primary blob endpoint URL."
-  value       = azurerm_storage_account.this.primary_blob_endpoint
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "primary_access_key" {
   description = "The primary access key of the storage account."
-  value       = azurerm_storage_account.this.primary_access_key
+  value       = azurerm_storage_account.sa.primary_access_key
   sensitive   = true
 }
 
 output "primary_connection_string" {
   description = "The primary connection string of the storage account."
-  value       = azurerm_storage_account.this.primary_connection_string
+  value       = azurerm_storage_account.sa.primary_connection_string
   sensitive   = true
 }
